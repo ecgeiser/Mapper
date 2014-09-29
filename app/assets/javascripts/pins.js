79056a1d56a8716b878c1333257f01ca47c80ece
ecgeiser <erica.geiser@gmail.com>
2014-09-29 18:46:34 -0400

    edited controllers and added ajax requests

--- app/assets/javascripts/pins.js
+++ app/assets/javascripts/pins.js
@@ -1,10 +1,51 @@
 function initialize() {
   var mapOptions = {
-    center: { lat: 38.8951100	, lng: -77.0363700
-},
-     zoom: 8
+    center: { lat: 38.8951100	, lng: -77.0363700 },
+    zoom: 8
   };
   var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+$.ajax ({
+	type: "GET",
+	url: "/pins",
+	dataType: "json",
+	success: function(data) {
+		for (var i = 0; i < data.length; i++) {
+			new google.maps.Marker({
+  		position: new google.maps.LatLng(data[i].latitude, data[i].longitude),
+  		map: map
+		});
+		}
+
+	},
+});
+
+	google.maps.event.addListener(map, 'click', function(event) {
+	  var location = event.latLng;
+
+	  $.ajax({
+	    type: "POST",
+	    url: "/pins",
+	    dataType: "json",
+	    data: { 
+	    	pin: {
+	    		longitude: location.k, 
+	    		latitude: location.A
+	    	}
+	     },
+	    success: function(data) {
+	    	console.log(data)
+	      new google.maps.Marker({
+	      	position: new google.maps.LatLng(data.latitude, data.longitude),
+	      	map: map
+	      });
+	    } 
+		});
+	});
+
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
+
+
+
--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -2,10 +2,26 @@ class PinsController < ApplicationController
 
 def index
 	@pins = Pin.all
+	respond_to do |format|
+		format.html
+		format.json { render :json => @pins }
+	end
+end
+
+def new
+	@pin = Pin.new
 end
 
 def create
 	@pin = Pin.new(pin_params)
+  if @pin.save
+    respond_to do |format|
+      format.html
+      format.json { render :json => @pin }
+    end
+  else
+    render :index
+  end
 end
 
 private
